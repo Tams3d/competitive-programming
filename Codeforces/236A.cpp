6e4202ad7a6f3db9f564c8099150f90ba041c5a9
Tamil Selvan <106262964+Tams3d@users.noreply.github.com>
2026-01-08 07:27:22 +0530

    so it was on 7.1.26 btw

---
+++ Codeforces/236A.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string s;
+    cin >> s;
+
+    int freq[26] = { 0 };
+    int distinct = 0;
+    for (char c : s)
+        freq[c - 'a']++;
+
+    for (int x : freq)
+        if (x > 0)
+            distinct++;
+
+    if (distinct & 1)
+        cout << "IGNORE HIM!";
+    else
+        cout << "CHAT WITH HER!";
+}
---
+++ Codeforces/339A.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string s;
+    cin >> s;
+    int count[3] = { 0 };
+
+    for (char c : s)
+        if (c != '+')
+            ++count[c - '1'];
+
+    bool first = true;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < count[i]; j++) {
+            if (!first)
+                cout << '+';
+            cout << i + 1;
+            first = false;
+        }
+    }
+    cout << '\n';
+}
