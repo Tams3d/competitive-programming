5dc270a7a53478eb56e06cffe8d669691957b502
Tamil Selvan <106262964+Tams3d@users.noreply.github.com>
2026-01-13 23:59:22 +0530

    lets domore - 13.1.26

---
+++ Codeforces/122A.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool is_lucky(int x) {
+  while (x > 0) {
+    int last = x % 10;
+    if (last != 4 && last != 7)
+      return false;
+    x /= 10;
+  }
+  return true;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  bool flag = false;
+
+  for (int i = 1; i <= n; i++) {
+    if (is_lucky(i) && n % i == 0) {
+      flag = true;
+      break;
+    }
+  }
+
+  if (flag)
+    cout << "YES";
+  else
+    cout << "NO";
+
+  return 0;
+}
